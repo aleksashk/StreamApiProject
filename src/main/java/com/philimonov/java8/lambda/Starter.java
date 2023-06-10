7edcd55a83a366d06183bb091eeef3042dc3dc48
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-06-10 17:59:08 +0300

    refactoring

--- src/main/java/com/philimonov/java8/lambda/Starter.java
+++ src/main/java/com/philimonov/java8/lambda/Starter.java
@@ -2,6 +2,7 @@ package com.philimonov.java8.lambda;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Starter {
@@ -9,7 +10,7 @@ public class Starter {
         List<String> names = Arrays.asList("peter", "anna", "mike", "xenia");
         System.out.println(names);
 
-        Collections.sort(names, (o1, o2) -> o2.compareTo(o1));
+        Collections.sort(names, (a, b) -> b.compareTo(a));
         System.out.println(names);
     }
 }

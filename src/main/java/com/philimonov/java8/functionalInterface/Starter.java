bf0ebedc5d7cb889cee9fffdf283f336e3b8df6d
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-06-10 18:05:32 +0300

    references to methods and constructors

--- src/main/java/com/philimonov/java8/functionalInterface/Starter.java
+++ src/main/java/com/philimonov/java8/functionalInterface/Starter.java
@@ -2,7 +2,7 @@ package com.philimonov.java8.functionalInterface;
 
 public class Starter {
     public static void main(String[] args) {
-        Converter<String, Integer> converter = (from) -> Integer.valueOf(from);
+        Converter<String, Integer> converter = Integer::valueOf;
         Integer converted = converter.convert("123");
         System.out.println(converted);
     }
